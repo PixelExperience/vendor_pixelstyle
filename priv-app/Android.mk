23631f45d0e038f1f4ceb255d4155a0d8e5deffb
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-07 21:16:17 -0200

    WallpaperPickerGooglePrebuilt: Move to /app

--- priv-app/Android.mk
+++ priv-app/Android.mk
@@ -25,15 +25,3 @@ LOCAL_OVERRIDES_PACKAGES := Launcher3
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := WallpaperPickerGooglePrebuilt
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := WallpaperPickerGooglePrebuilt/WallpaperPickerGooglePrebuilt.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := WallpaperPicker \
-                            WallpaperCropper
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
