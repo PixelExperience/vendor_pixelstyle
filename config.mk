56e1b138ad8b863ef427e2ac9b805de6fbeab7d1
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-08-09 20:20:38 -0300

    Don't build Turbo for now
    * We don't have a proper implementation on SystemUI and Settings (found on SettingsGoogle from taimen)
    
    Change-Id: I199663261c4a9364a02e87d769de650c4685eb71

--- config.mk
+++ config.mk
@@ -18,7 +18,6 @@ PRODUCT_PACKAGES += \
     WallpaperPickerGooglePrebuilt \
     NexusLauncherPrebuilt \
     TimeZoneDataPrebuilt \
-    TurboPrebuilt \
     WellbeingPrebuilt \
     MatchmakerPrebuilt
 
