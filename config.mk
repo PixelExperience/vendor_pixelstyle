8e5fef87bfdf0e6f9d761c88871d15349f5f3372
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-07 20:24:16 -0200

    WeatherClient: Bump revision to add support for new icons
    Change-Id: Icfdb9fb78a23b95770c5cb94b7882e7f5497f191

--- config.mk
+++ config.mk
@@ -80,5 +80,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     vendor/pixelstyle/overlay/common/
 
 # Weather
+PRODUCT_PROPERTY_OVERRIDES += \
+    org.pixelexperience.weather.revision=2
+
 PRODUCT_COPY_FILES += \
     vendor/pixelstyle/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
