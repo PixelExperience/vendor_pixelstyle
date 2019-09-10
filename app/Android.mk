853c8ef4ea18f66a5cbb9937aeb900d687c8bcda
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 00:57:39 -0300

    Cleanup makefile
    Change-Id: I5cb81c23adda7f288ab190242936287f6d4bf728

--- app/Android.mk
+++ app/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := EmergencyInfoGoogleNoUi
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := EmergencyInfoGoogleNoUi/EmergencyInfoGoogleNoUi.apk
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
@@ -14,7 +14,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := NexusWallpapersStubPrebuilt2018
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := NexusWallpapersStubPrebuilt2018/NexusWallpapersStubPrebuilt2018.apk
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
